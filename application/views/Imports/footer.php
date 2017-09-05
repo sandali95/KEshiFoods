28b90c7f021417ba378c95d9d818d317bd1e84a0
sandali95 <wathmali123@gmail.com>
2017-09-05 21:24:38 +0530

    no message

---
+++ application/views/Imports/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer" xmlns="http://www.w3.org/1999/html">
+    <div class="pull-right hidden-xs">
+
+    </div>
+    <strong>Copyright &copy; 2017-2019</strong></br>
+    <b>Keshi Foods    </b>     All rights
+    reserved.
+</footer>
