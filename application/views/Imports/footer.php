7f418f31e2f5a9d2aa22720186b66b424a53d755
Sandali <wathmali123@gmail.com>
2017-10-21 13:12:58 +0530

    refreshRepo

--- application/views/Imports/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="main-footer" xmlns="http://www.w3.org/1999/html">
-    <div class="pull-right hidden-xs">
-
-    </div>
-    <strong>Copyright &copy; 2017-2019</strong></br>
-    <b>Keshi Foods    </b>     All rights
-    reserved.
-</footer>
