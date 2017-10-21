729922e1920c2ed62bd3c4b9bb84ed62587f895c
Buddhika Shaminda Rathnayaka <rmbsrathnayaka@gmail.com>
2017-10-21 21:46:07 +0530

    add employee

---
+++ application/controllers/Admin.php
@@ -0,0 +1,49 @@
+<?php
+class Admin extends CI_Controller{
+	public function index(){
+		//$res=$this->get_data();
+
+		$this->load->view('em');
+		//echo "$result";
+	}
+
+	public function emp_register(){
+
+		$this->form_validation->set_rules('name','Name','required');
+		$this->form_validation->set_rules('gender','Gender','required');
+		$this->form_validation->set_rules('cnumber','contact','required');
+		$this->form_validation->set_rules('address','Address','required');
+		$this->form_validation->set_rules('etype','Employee Type','required');
+
+		if($this->form_validation->run()==True){
+			$this->load->model('model_admin');
+			$this->model_admin->insertdata();
+			//$this->get_data();
+			$this->load->model('model_admin');
+			$data=$this->model_admin->fetch_data();
+			$this->load->view('view_data',array('data' => $data));
+
+			
+	
+		}
+		else{
+			$this->load->view('emp_reg.php');
+		}
+	}
+	public function get_data(){
+		$this->load->model('model_admin');
+		$fetch_data=$this->model_admin->fetch_data();
+		$data =array(
+			'emp'=>$fetch_data
+		);
+		$this->load->view('view_data',$data);
+		//return $fetch_data;
+	}
+	public function search_data(){
+		$this->form_validation->set_rules('name','Name','required');
+		if($this->form_validation->run()==True){
+			$this->load->model('model_admin');
+			$this->model_admin->search_data();
+		}
+	}
+}
