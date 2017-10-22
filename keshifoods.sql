046a89b43fb8f55d8a1a71767cd4bb9f31dc11f3
Sandali <wathmali123@gmail.com>
2017-10-22 23:01:50 +0530

    databaseUpdated

--- keshifoods.sql
+++ keshifoods.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Oct 14, 2017 at 08:39 PM
--- Server version: 5.7.14
--- PHP Version: 5.6.25
+-- Host: 127.0.0.1:3306
+-- Generation Time: Oct 22, 2017 at 03:59 PM
+-- Server version: 5.7.19
+-- PHP Version: 5.6.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -26,11 +28,13 @@ SET time_zone = "+00:00";
 -- Table structure for table `advanced_payment`
 --
 
-CREATE TABLE `advanced_payment` (
+DROP TABLE IF EXISTS `advanced_payment`;
+CREATE TABLE IF NOT EXISTS `advanced_payment` (
   `emp_id` int(11) NOT NULL,
   `month` varchar(10) NOT NULL,
   `date` date DEFAULT NULL,
-  `amount` int(11) DEFAULT NULL
+  `amount` int(11) DEFAULT NULL,
+  PRIMARY KEY (`emp_id`,`month`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -50,11 +54,13 @@ INSERT INTO `advanced_payment` (`emp_id`, `month`, `date`, `amount`) VALUES
 -- Table structure for table `bill`
 --
 
-CREATE TABLE `bill` (
+DROP TABLE IF EXISTS `bill`;
+CREATE TABLE IF NOT EXISTS `bill` (
   `bill_no` int(11) NOT NULL,
   `date` date DEFAULT NULL,
   `customer_name` varchar(45) DEFAULT NULL,
-  `total_val` int(11) DEFAULT NULL
+  `total_val` int(11) DEFAULT NULL,
+  PRIMARY KEY (`bill_no`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -71,12 +77,15 @@ INSERT INTO `bill` (`bill_no`, `date`, `customer_name`, `total_val`) VALUES
 -- Table structure for table `bill_item`
 --
 
-CREATE TABLE `bill_item` (
+DROP TABLE IF EXISTS `bill_item`;
+CREATE TABLE IF NOT EXISTS `bill_item` (
   `bill_no` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
   `qty` int(11) DEFAULT NULL,
   `unit_price` int(11) DEFAULT NULL,
-  `total_val_perItem` varchar(45) DEFAULT NULL
+  `total_val_perItem` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`bill_no`,`product_id`),
+  KEY `bill_item_product_No_idx` (`product_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -99,21 +108,25 @@ INSERT INTO `bill_item` (`bill_no`, `product_id`, `qty`, `unit_price`, `total_va
 -- Table structure for table `employee`
 --
 
-CREATE TABLE `employee` (
+DROP TABLE IF EXISTS `employee`;
+CREATE TABLE IF NOT EXISTS `employee` (
   `emp_id` int(11) NOT NULL,
   `emp_name` varchar(50) DEFAULT NULL,
+  `emp_gender` varchar(11) NOT NULL,
   `emp_address` varchar(100) DEFAULT NULL,
   `emp_contact_num` int(11) DEFAULT NULL,
-  `emp_type` varchar(10) DEFAULT NULL
+  `emp_type` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`emp_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `employee`
 --
 
-INSERT INTO `employee` (`emp_id`, `emp_name`, `emp_address`, `emp_contact_num`, `emp_type`) VALUES
-(1000, 'Hewawissa', 'Gampaha', 332222222, 'Manager'),
-(1001, 'Kamal', 'Colombo', 712222222, 'worker');
+INSERT INTO `employee` (`emp_id`, `emp_name`, `emp_gender`, `emp_address`, `emp_contact_num`, `emp_type`) VALUES
+(1, 'Shaminda', 'Male', 'Godakawela', 771114723, 'Manager'),
+(1000, 'Hewawissa', '0', 'Gampaha', 332222222, 'Manager'),
+(1001, 'Kamal', '0', 'Colombo', 712222222, 'worker');
 
 -- --------------------------------------------------------
 
@@ -121,10 +134,12 @@ INSERT INTO `employee` (`emp_id`, `emp_name`, `emp_address`, `emp_contact_num`, 
 -- Table structure for table `product`
 --
 
-CREATE TABLE `product` (
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE IF NOT EXISTS `product` (
   `item_id` int(11) NOT NULL,
   `item_name` varchar(45) NOT NULL,
-  `item_price` int(11) DEFAULT NULL
+  `item_price` int(11) DEFAULT NULL,
+  PRIMARY KEY (`item_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -155,11 +170,13 @@ INSERT INTO `product` (`item_id`, `item_name`, `item_price`) VALUES
 -- Table structure for table `purchase`
 --
 
-CREATE TABLE `purchase` (
+DROP TABLE IF EXISTS `purchase`;
+CREATE TABLE IF NOT EXISTS `purchase` (
   `stock_id` int(10) NOT NULL,
   `invoice_num` varchar(25) NOT NULL,
   `invoice_date` date NOT NULL,
-  `item_quantity` float NOT NULL
+  `item_quantity` float NOT NULL,
+  PRIMARY KEY (`invoice_num`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -177,11 +194,13 @@ INSERT INTO `purchase` (`stock_id`, `invoice_num`, `invoice_date`, `item_quantit
 -- Table structure for table `salary_payment`
 --
 
-CREATE TABLE `salary_payment` (
+DROP TABLE IF EXISTS `salary_payment`;
+CREATE TABLE IF NOT EXISTS `salary_payment` (
   `emp_id` int(11) NOT NULL,
   `month` varchar(10) NOT NULL,
   `date` date DEFAULT NULL,
-  `amount` int(11) NOT NULL
+  `amount` int(11) NOT NULL,
+  PRIMARY KEY (`emp_id`,`month`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -200,10 +219,12 @@ INSERT INTO `salary_payment` (`emp_id`, `month`, `date`, `amount`) VALUES
 -- Table structure for table `stock`
 --
 
-CREATE TABLE `stock` (
+DROP TABLE IF EXISTS `stock`;
+CREATE TABLE IF NOT EXISTS `stock` (
   `stock_id` int(11) NOT NULL,
   `stock_name` varchar(45) DEFAULT NULL,
-  `tot_quantity` int(11) NOT NULL
+  `tot_quantity` int(11) NOT NULL,
+  PRIMARY KEY (`stock_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -221,9 +242,11 @@ INSERT INTO `stock` (`stock_id`, `stock_name`, `tot_quantity`) VALUES
 -- Table structure for table `users`
 --
 
-CREATE TABLE `users` (
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
   `username` varchar(50) NOT NULL,
-  `userpass` varchar(45) NOT NULL
+  `userpass` varchar(45) NOT NULL,
+  PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -232,65 +255,6 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`username`, `userpass`) VALUES
 ('admin@keshi', 'admin@keshi');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `advanced_payment`
---
-ALTER TABLE `advanced_payment`
-  ADD PRIMARY KEY (`emp_id`,`month`);
-
---
--- Indexes for table `bill`
---
-ALTER TABLE `bill`
-  ADD PRIMARY KEY (`bill_no`);
-
---
--- Indexes for table `bill_item`
---
-ALTER TABLE `bill_item`
-  ADD PRIMARY KEY (`bill_no`,`product_id`),
-  ADD KEY `bill_item_product_No_idx` (`product_id`);
-
---
--- Indexes for table `employee`
---
-ALTER TABLE `employee`
-  ADD PRIMARY KEY (`emp_id`);
-
---
--- Indexes for table `product`
---
-ALTER TABLE `product`
-  ADD PRIMARY KEY (`item_id`);
-
---
--- Indexes for table `purchase`
---
-ALTER TABLE `purchase`
-  ADD PRIMARY KEY (`invoice_num`);
-
---
--- Indexes for table `salary_payment`
---
-ALTER TABLE `salary_payment`
-  ADD PRIMARY KEY (`emp_id`,`month`);
-
---
--- Indexes for table `stock`
---
-ALTER TABLE `stock`
-  ADD PRIMARY KEY (`stock_id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`username`);
 
 --
 -- Constraints for dumped tables
@@ -314,6 +278,7 @@ ALTER TABLE `bill_item`
 --
 ALTER TABLE `salary_payment`
   ADD CONSTRAINT `sal_pay_emp` FOREIGN KEY (`emp_id`) REFERENCES `employee` (`emp_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
